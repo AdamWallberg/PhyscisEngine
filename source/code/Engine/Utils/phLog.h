558979b894253e64c415291dc6af890324d250de
Adam Wallberg <saddam.wallberg@gmail.com>
2016-09-17 16:53:18 +0200

    Add macro for logging

---
+++ source/code/Engine/Utils/phLog.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <stdio.h>
+
+// TODO: Replace printf with own logging methods
+// that can save data to files, and exit the program. 
+
+// Regular log message
+#define _log( MESSAGE, ... ) printf( "%s\n", MESSAGE, __VA_ARGS__ )
+
+// Debug log message
+#ifdef DEBUG
+#define _logDebug( MESSAGE, ... ) printf( "%s\n", MESSAGE, __VA_ARGS__ )
+#else
+#define _logDebug( MESSAGE, ... )
+#endif
