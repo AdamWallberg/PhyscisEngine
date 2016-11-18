89b64455ff2ca915027ee34252ef71690beefa16
Notebook <adamwallbergcontact@gmail.com>
2016-11-18 20:06:30 +0100

    Add basic clock class, with support for custom stopwatches

---
+++ source/code/Engine/Clock/phCClock.cpp
@@ -0,0 +1,73 @@
+#include "phCClock.h"
+#include "Engine/phSystem.h"
+
+#include <GLFW/glfw3.h>
+
+phCClock::phCClock()
+	: m_paused( false )
+	, m_deltaTime( 0.0 )
+	, m_deltaTimeReal( 0.0 )
+	, m_lifeTime( 0.0 )
+	, m_lifeTimeReal( 0.0 )
+{
+	m_lastTime = glfwGetTime();
+}
+
+phCClock::~phCClock()
+{
+	for( auto it : m_clocks )
+	{
+		_logError( "Found unremoved clock: %s. Please call StopStopwatch() when done using it, for increased performance.", it.first );
+	}
+
+	m_clocks.clear();
+}
+
+
+
+void phCClock::Update()
+{
+	const double newTime = glfwGetTime();
+	m_deltaTimeReal = newTime - m_lastTime;
+	m_lastTime = newTime;
+	m_deltaTime = m_paused ? 0.0 : m_deltaTimeReal;
+
+	m_lifeTimeReal += m_deltaTimeReal;
+	m_lifeTime += m_deltaTime;
+
+	for( auto& it : m_clocks )
+	{
+		it.second.time += it.second.isPaused ? 0.0 : m_deltaTimeReal;
+	}
+}
+
+
+
+void phCClock::StartStopwatch( const char* name )
+{
+	m_clocks[ name ] = { 0.0, false };
+}
+
+const float& phCClock::GetStopwatchTime( const char* name )
+{
+	return static_cast<float>( m_clocks[ name ].time );
+}
+
+const float& phCClock::PauseStopwatch( const char* name )
+{
+	m_clocks[ name ].isPaused = true;
+	return static_cast<float>( m_clocks[ name ].time );
+}
+
+const float& phCClock::ResumeStopwatch( const char* name )
+{
+	m_clocks[ name ].isPaused = false;
+	return static_cast<float>( m_clocks[ name ].time );
+}
+
+const float& phCClock::StopStopwatch( const char* name )
+{
+	const float value = m_clocks[ name ].time;
+	m_clocks.erase( name );
+	return static_cast<float>( value );
+}
---
+++ source/code/Engine/Clock/phCClock.h
@@ -0,0 +1,52 @@
+#pragma once
+
+#include "Engine/Utils/phCSingleton.h"
+
+#include <map>
+
+// Keeps track of the total time of the application,
+// whether or not the application is paused.
+// And can setup and update custom clocks and timers.
+class phCClock : public phCSingleton<phCClock>
+{
+public:
+	phCClock();
+	~phCClock();
+
+	void Update();
+
+	// All stopwatch methods that return a float value 
+	// will return -1.0f if the stopwatch doesn't exist.
+
+	// Starts a custom stopwatch.
+	// Remember to call StopStopwatch when done using it.
+	void StartStopwatch( const char* name );
+	// Recieves the elapsed time since stopwatch was created.
+	const float& GetStopwatchTime( const char* name );
+	// Pauses the stopwatch and returns its current elapsed time.
+	const float& PauseStopwatch( const char* name );
+	// Resumes the stopwatch and returns its current elapsed time.
+	const float& ResumeStopwatch( const char* name );
+	// Removes the stopwatch, and returns the final elapsed time.
+	const float& StopStopwatch( const char* name );
+
+	const float& GetLifeTime() { return static_cast<float>( m_lifeTime ); }
+	const float& GetLifeTimeReal() { return static_cast<float>( m_lifeTimeReal ); }
+	const float& GetDeltaTime() { return static_cast<float>( m_deltaTime ); }
+	const float& GetDeltaTimeReal() { return static_cast<float>( m_deltaTimeReal ); }
+
+private:
+	struct SStopwatch
+	{
+		double time;
+		bool isPaused;
+	};
+
+	bool m_paused;
+	double m_lastTime;
+	double m_lifeTime;
+	double m_lifeTimeReal;
+	double m_deltaTime;
+	double m_deltaTimeReal;
+	std::map<const char* /*name*/, SStopwatch> m_clocks;
+};
