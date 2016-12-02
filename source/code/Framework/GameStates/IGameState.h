1b14640890b3f3049467f35b5cccfec4b72b9c2f
Notebook <adamwallbergcontact@gmail.com>
2016-12-02 21:01:21 +0100

    Create interface for game states

---
+++ source/code/Framework/GameStates/IGameState.h
@@ -0,0 +1,30 @@
+#pragma once
+
+class CGameStateMachine;
+
+class IGameState
+{
+public:
+
+	IGameState( CGameStateMachine* pMachine )
+		: m_pMachine( pMachine )
+	{
+	}
+
+	// These methods run during the loading screen.
+	// It's recommended to allocate/deallocate memory,
+	// and load assets in these, and not the enter and exit methods.
+	virtual void OnCreate() = 0;
+	virtual void OnDestroy() = 0;
+	
+	// Runs after loading is done, and game state is entered.
+	virtual void OnEnter() = 0;
+	// Runs before loading screen, when state is exited.
+	virtual void OnExit() = 0;
+	virtual void Update() = 0;
+
+private:
+
+	CGameStateMachine* m_pMachine;
+
+};
