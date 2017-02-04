0c809beffa4741bfd82a5bb453e786d00b8af69e
Adam Wallberg <adamwallbergcontact@gmail.com>
2017-02-04 22:35:43 +0100

    Add headers for index and vertex buffers

---
+++ source/code/Engine/Data/Models/phCIndexBuffer.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "phSystem.h"
+#include <vector>
+
+class phCIndexBuffer
+{
+public:
+	phCIndexBuffer();
+
+private:
+
+	friend class phCFileSystem;
+
+};
---
+++ source/code/Engine/Data/Models/phCVertexBuffer.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <vector>
+#include "Math/pmV4.h"
+#include "Math/pmMat4.h"
+
+struct SVertex
+{
+	pmV3 position;
+	pmV3 normal;
+	pmV2 uv;
+	pmV4 color;
+};
+
+class phCVertexBuffer
+{
+public:
+
+	phCVertexBuffer();
+
+private:
+
+	std::vector<SVertex> m_vertices;
+
+	friend class phCFileSystem;
+
+};
