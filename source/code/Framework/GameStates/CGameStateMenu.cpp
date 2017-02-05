5666c3342d6bd76bda2fb4d1d7edbedfc54ace80
Adam Wallberg <adamwallbergcontact@gmail.com>
2017-02-05 13:46:55 +0100

    Switch to game state game after 4 seconds of runtime

--- source/code/Framework/GameStates/CGameStateMenu.cpp
+++ source/code/Framework/GameStates/CGameStateMenu.cpp
@@ -1,5 +1,7 @@
 #include "CGameStateMenu.h"
 #include "phSystem.h"
+#include "Clock/phCClock.h"
+#include "CGameStateMachine.h"
 
 CGameStateMenu::CGameStateMenu( CGameStateMachine* pMachine )
 	: IGameState( pMachine )
@@ -22,14 +24,19 @@ void CGameStateMenu::OnDestroy()
 void CGameStateMenu::OnEnter()
 {
 	_logDebug( "Game State MENU: ON ENTER" );
+	phCClock::GetInstance().StartStopwatch("game_state_menu_lifetime");
 }
 
 void CGameStateMenu::OnExit()
 {
 	_logDebug( "Game State MENU: ON EXIT" );
+	phCClock::GetInstance().StopStopwatch("game_state_menu_lifetime");
 }
 
 void CGameStateMenu::Update()
 {
-
+	if(phCClock::GetInstance().GetStopwatchTime("game_state_menu_lifetime") >= 4.0f)
+	{
+		m_pMachine->ChangeGameState("game");
+	}
 }
