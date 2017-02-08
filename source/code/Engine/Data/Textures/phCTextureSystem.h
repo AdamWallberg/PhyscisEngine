b569ec70b8d1a9b4d0508830e29c7e7e56e8224c
Adam Wallberg <adamwallbergcontact@gmail.com>
2017-02-08 23:47:34 +0100

    Create texture system header

---
+++ source/code/Engine/Data/Textures/phCTextureSystem.h
@@ -0,0 +1,18 @@
+#pragma once
+#include "Systems/phServiceLocators.h"
+
+class phCTextureSystem
+{
+public:
+
+
+
+private:
+
+
+
+};
+
+class phCTextureSystemLocator : public phIServiceLocator<phCTextureSystem>
+{
+};
